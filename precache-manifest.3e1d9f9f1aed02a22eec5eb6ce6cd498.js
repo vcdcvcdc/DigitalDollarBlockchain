f1bbcf2ceb893930d38ba90623f7983b3b1a9f40
Smart133594 <happylife97411@gmail.com>
2023-06-13 13:10:07 +0700

    Updated

---
+++ precache-manifest.3e1d9f9f1aed02a22eec5eb6ce6cd498.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beb88df9dff5b361a7d3c439f0f0415e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cacdcf1018009debd62",
+    "url": "/static/css/main.fdd1d40c.chunk.css"
+  },
+  {
+    "revision": "07612ff3c8cfb389ce45",
+    "url": "/static/js/2.158534c3.chunk.js"
+  },
+  {
+    "revision": "539d4d7c271f2aacb62d8887d608815b",
+    "url": "/static/js/2.158534c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cacdcf1018009debd62",
+    "url": "/static/js/main.6f47b68a.chunk.js"
+  },
+  {
+    "revision": "b179efbd6c2fc35042e3",
+    "url": "/static/js/runtime-main.350b9303.js"
+  }
+]);
