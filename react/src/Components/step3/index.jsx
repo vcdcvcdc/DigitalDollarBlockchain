acbf8acccb350fd5edd9fa9295c685a64787e845
ralphskaf <ralphskaff97@gmail.com>
2023-06-24 18:06:05 +0300

    flow working to accept kyc and get karma

--- react/src/Components/step3/index.jsx
+++ react/src/Components/step3/index.jsx
@@ -5,7 +5,6 @@ import WWDetailsContext from "../../contexts/wwDetailsContext";
 const Step2 = () => {
   const [address, setAddress] = useState("");
   const { wwDetails } = useContext(WWDetailsContext);
-
   function addAccountKyc() {
     const url =
       "https://backend.ddbc.dev/api/v1/account/add/" + wwDetails?.userAddress;
@@ -14,6 +13,55 @@ const Step2 = () => {
       .post(url)
       .then((response) => {
         console.log("Request successful");
+        getAllAccounts();
+        // Handle the response here
+      })
+      .catch((error) => {
+        console.error("Error occurred:", error);
+        // Handle any errors here
+      });
+  }
+
+  function getAllAccounts() {
+    const url = "https://backend.ddbc.dev/api/v1/account/get_all";
+
+    axios
+      .get(url)
+      .then((response) => {
+        console.log("Request successful", response?.data?.data);
+        // Handle the response here
+        const item = findItemWithAddress(
+          response?.data?.data,
+          wwDetails?.userAddress
+        );
+        console.log(item, "item");
+      })
+      .catch((error) => {
+        console.error("Error occurred:", error);
+        // Handle any errors here
+      });
+  }
+
+  function findItemWithAddress(arr, targetAddress) {
+    const foundItem = arr.find((item) => item.address === targetAddress);
+    if (foundItem) {
+      acceptAccount(foundItem);
+      return foundItem;
+    }
+
+    return null; // If no item with the specified address is found
+  }
+
+  function acceptAccount(foundItem) {
+    const url = `https://backend.ddbc.dev/api/v1/account/update/${foundItem?.id}`;
+    const data = {
+      status: 1, //-1 for rejected
+    };
+
+    axios
+      .post(url, data)
+      .then((response) => {
+        console.log("accept account", response);
         // Handle the response here
       })
       .catch((error) => {
