226ccfd8b281463b9ff60502bf706931969f88c2
anthony salameh <anthonysalameh@anthonys-MacBook-Pro.local>
2023-06-26 15:03:26 +0300

    header tabs fully functional

--- react/src/Components/Player.jsx
+++ react/src/Components/Player.jsx
@@ -2,13 +2,9 @@ import React, { Component } from "react";
 import { gsap } from "gsap";
 import { SplitText } from "gsap/SplitText";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import Step6 from "./step6";
-import Step1 from "./step1";
-import Step2 from "./step2";
 import SpaceDomination from "./spaceDomination";
-import DescriptionSection from "./descriptionSection";
 import IntroSection from "./introSection";
-import HeaderMenu from "./headerMenu";
+import ContentTabs from "./contentTabs";
 gsap.registerPlugin(SplitText, ScrollTrigger);
 
 class Player extends Component {
@@ -33,82 +29,7 @@ class Player extends Component {
         <div id="app">
           <SpaceDomination />
 
-          <section id="header">
-            <div className="wrap">
-              <div className="ui-inner-border t"></div>
-
-              <div className="ui-grid">
-                <div className="ui-box">
-                  <div className="text-container">
-                    <svg>
-                      <symbol id="s-text">
-                        <text textAnchor="middle" x="50%" y="80%">
-                          Digital Dollar
-                        </text>
-                      </symbol>
-                      <g className="g-ants">
-                        <use xlinkHref="#s-text" className="text-copy"></use>
-                        <use xlinkHref="#s-text" className="text-copy"></use>
-                        <use xlinkHref="#s-text" className="text-copy"></use>
-                        <use xlinkHref="#s-text" className="text-copy"></use>
-                        <use xlinkHref="#s-text" className="text-copy"></use>
-                      </g>
-                    </svg>
-                  </div>
-                  <div id="description">
-                    <DescriptionSection />
-
-                    {/* STEPS TO GET STARTED */}
-                    <div className="text-box" id="login">
-                      <div className="scroll-content">
-                        <svg viewBox="0 -20 1320 70">
-                          <text x="50%" y="50%" textAnchor="middle">
-                            Testing DDBC:
-                          </text>
-                        </svg>
-                        <Step1 />
-                        <Step2 />
-                        <Step6 />
-                      </div>
-                    </div>
-                  </div>
-                  <div className="ui-fx">
-                    <div className="ui-inner-border l"></div>
-                    <div className="ui-inner-border r"></div>
-                    {[1, 2, 3, 4].map(function (n) {
-                      return (
-                        <span
-                          className={"ui-corner"}
-                          key={n * Math.random()}
-                        ></span>
-                      );
-                    })}
-
-                    {["l", "r", "t", "b"].map(function (n) {
-                      return (
-                        <div
-                          className={"ui-batts " + n}
-                          key={n + Math.random()}
-                        >
-                          {[1, 2].map(function (n) {
-                            return (
-                              <span
-                                className={"batt"}
-                                key={n * Math.random()}
-                              ></span>
-                            );
-                          })}
-                        </div>
-                      );
-                    })}
-                  </div>
-                </div>
-              </div>
-              <div className="ui-inner-border b">
-                <HeaderMenu />
-              </div>
-            </div>
-          </section>
+          <ContentTabs />
         </div>
       </>
     );
---
+++ react/src/Components/contentTabs/index.jsx
@@ -0,0 +1,123 @@
+//create a new component called ContentTabs
+import React, { useState } from "react";
+//import the css file
+import "../contentTabs/dist/contentTabs.css";
+
+
+import DescriptionSection from "../descriptionSection";
+import HeaderMenu from "../headerMenu";
+
+
+//create a new component called ContentTabs
+const ContentTabs = () => {
+    const [activeTab, setActiveTab] = useState("tab1");
+
+    return (
+        <>
+            <section id="header">
+                <div className="wrap">
+                    <div className="ui-inner-border t"></div>
+                    <div className="header-tabs text-center">
+                        <div className="row">
+                            <div className="col">
+                                <div className="pointer" onClick={() => setActiveTab("tab1")}>
+                                    test 1
+                                </div>
+                            </div>
+                            <div className="col">
+                                <div className="pointer" onClick={() => setActiveTab("tab2")}>
+                                    test 2
+                                </div>
+                            </div>
+                            <div className="col">
+                                <div className="pointer" onClick={() => setActiveTab("tab3")}>
+                                    test 3
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="ui-grid">
+                        <div className="ui-box">
+
+                            <div className="text-container">
+                                <svg>
+                                    <symbol id="s-text">
+                                        <text textAnchor="middle" x="50%" y="80%">
+                                            Digital Dollar
+                                        </text>
+                                    </symbol>
+                                    <g className="g-ants">
+                                        <use xlinkHref="#s-text" className="text-copy"></use>
+                                        <use xlinkHref="#s-text" className="text-copy"></use>
+                                        <use xlinkHref="#s-text" className="text-copy"></use>
+                                        <use xlinkHref="#s-text" className="text-copy"></use>
+                                        <use xlinkHref="#s-text" className="text-copy"></use>
+                                    </g>
+                                </svg>
+                            </div>
+
+
+
+                            <div id="description">
+                                <DescriptionSection
+                                    setActiveTab={setActiveTab}
+                                    activeTab={activeTab}
+                                />
+
+
+                            </div>
+
+
+
+
+
+
+
+
+                            <div className="ui-fx">
+                                <div className="ui-inner-border l"></div>
+                                <div className="ui-inner-border r"></div>
+                                {[1, 2, 3, 4].map(function (n) {
+                                    return (
+                                        <span
+                                            className={"ui-corner"}
+                                            key={n * Math.random()}
+                                        ></span>
+                                    );
+                                })}
+
+                                {["l", "r", "t", "b"].map(function (n) {
+                                    return (
+                                        <div
+                                            className={"ui-batts " + n}
+                                            key={n + Math.random()}
+                                        >
+                                            {[1, 2].map(function (n) {
+                                                return (
+                                                    <span
+                                                        className={"batt"}
+                                                        key={n * Math.random()}
+                                                    ></span>
+                                                );
+                                            })}
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        </div>
+                    </div>
+                    <div className="ui-inner-border b">
+                        <HeaderMenu />
+                    </div>
+                </div>
+            </section>
+
+
+
+
+
+
+        </>
+    );
+}
+export default ContentTabs;
