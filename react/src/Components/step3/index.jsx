9fe0e1f9542c5bfdde76c6a196f133b32991391d
ralphskaf <ralphskaff97@gmail.com>
2023-06-26 14:14:41 +0300

    bug fix

--- react/src/Components/step3/index.jsx
+++ react/src/Components/step3/index.jsx
@@ -3,105 +3,102 @@ import axios from "axios";
 import WWDetailsContext from "../../contexts/wwDetailsContext";
 
 const Step2 = () => {
-    const [address, setAddress] = useState("");
-    const { wwDetails } = useContext(WWDetailsContext);
+  const [address, setAddress] = useState("");
+  const { wwDetails } = useContext(WWDetailsContext);
 
-    useEffect(() => {
-        if (wwDetails?.userAddress) {
-            checkAccountStatus(wwDetails?.userAddress);
+  useEffect(() => {
+    if (wwDetails?.userAddress) {
+      checkAccountStatus(wwDetails?.userAddress);
+    }
+  }, [wwDetails?.userAddress]);
+
+  function checkAccountStatus(address) {
+    const url = "https://backend.ddbc.dev/api/v1/account/get_all";
+
+    axios
+      .get(url)
+      .then((response) => {
+        const item = response?.data?.data?.find(
+          (item) => item.address === address
+        );
+
+        if (item?.status === 1) {
+        } else {
+          addAccountKyc(address);
         }
-    }, [wwDetails?.userAddress]);
+      })
+      .catch((error) => {
+        console.error("Error occurred:", error);
+        // Handle any errors here
+      });
+  }
 
-    function checkAccountStatus(address) {
-        const url = "https://backend.ddbc.dev/api/v1/account/get_all";
+  function addAccountKyc(address) {
+    const url = `https://backend.ddbc.dev/api/v1/account/add/${address}`;
 
-        axios
-            .get(url)
-            .then((response) => {
-                const item = response?.data?.data?.find((item) => item.address === address);
+    axios
+      .post(url)
+      .then((response) => {
+        console.log("Request successful");
+        getAllAccounts();
+        // Handle the response here
+      })
+      .catch((error) => {
+        console.error("Error occurred:", error);
+        // Handle any errors here
+      });
+  }
 
-                if (item.status === 1) {
-                }
-                else {
-                    addAccountKyc(address)
-                }
-            })
-            .catch((error) => {
-                console.error("Error occurred:", error);
-                // Handle any errors here
-            });
+  function getAllAccounts() {
+    const url = "https://backend.ddbc.dev/api/v1/account/get_all";
+
+    axios
+      .get(url)
+      .then((response) => {
+        console.log("Request successful", response?.data?.data);
+        // Handle the response here
+        const item = findItemWithAddress(
+          response?.data?.data,
+          wwDetails?.userAddress
+        );
+        console.log(item, "item");
+      })
+      .catch((error) => {
+        console.error("Error occurred:", error);
+        // Handle any errors here
+      });
+  }
+
+  function findItemWithAddress(arr, targetAddress) {
+    const foundItem = arr?.find((item) => item?.address === targetAddress);
+    if (foundItem) {
+      acceptAccount(foundItem);
+      return foundItem;
     }
 
-    function addAccountKyc(address) {
-        const url = `https://backend.ddbc.dev/api/v1/account/add/${address}`;
+    return null; // If no item with the specified address is found
+  }
 
-        axios
-            .post(url)
-            .then((response) => {
-                console.log("Request successful");
-                getAllAccounts();
-                // Handle the response here
-            })
-            .catch((error) => {
-                console.error("Error occurred:", error);
-                // Handle any errors here
-            });
-    }
+  function acceptAccount(foundItem) {
+    const url = `https://backend.ddbc.dev/api/v1/account/update/${foundItem?.id}`;
+    const data = {
+      status: 1, //-1 for rejected
+    };
 
-    function getAllAccounts() {
-        const url = "https://backend.ddbc.dev/api/v1/account/get_all";
+    axios
+      .post(url, data)
+      .then((response) => {
+        console.log("accept account", response);
+        alert("Congratulations! You have been whitelisted!");
+        // Handle the response here
+      })
+      .catch((error) => {
+        console.error("Error occurred:", error);
+        // Handle any errors here
+      });
+  }
 
-        axios
-            .get(url)
-            .then((response) => {
-                console.log("Request successful", response?.data?.data);
-                // Handle the response here
-                const item = findItemWithAddress(
-                    response?.data?.data,
-                    wwDetails?.userAddress
-                );
-                console.log(item, "item");
-            })
-            .catch((error) => {
-                console.error("Error occurred:", error);
-                // Handle any errors here
-            });
-    }
-
-    function findItemWithAddress(arr, targetAddress) {
-        const foundItem = arr.find((item) => item.address === targetAddress);
-        if (foundItem) {
-            acceptAccount(foundItem);
-            return foundItem;
-        }
-
-        return null; // If no item with the specified address is found
-    }
-
-    function acceptAccount(foundItem) {
-        const url = `https://backend.ddbc.dev/api/v1/account/update/${foundItem?.id}`;
-        const data = {
-            status: 1, //-1 for rejected
-        };
-
-        axios
-            .post(url, data)
-            .then((response) => {
-                console.log("accept account", response);
-                alert("Congratulations! You have been whitelisted!");
-                // Handle the response here
-            })
-            .catch((error) => {
-                console.error("Error occurred:", error);
-                // Handle any errors here
-            });
-    }
-
-    return (
-        <div>
-
-        </div>
-    );
+  return <div></div>;
 };
 
 export default Step2;
