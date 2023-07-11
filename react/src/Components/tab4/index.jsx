e78160e07b75804fb02321e779b453d4e144fe63
anthony salameh <anthonysalameh@anthonys-MacBook-Pro.local>
2023-07-11 22:48:17 +0300

    fixed tokens layout and added the icons

--- react/src/Components/tab4/index.jsx
+++ react/src/Components/tab4/index.jsx
@@ -12,37 +12,88 @@ function Tab4() {
               Our Tokens
             </text>
           </svg>
-          <h3>Digital Dollar (DD)</h3>
-          <Text as="p" style={{ color: "#ddd" }}>
-            The DD is a digital asset uniting the stability of the US Dollar
-            with the prowess of blockchain technology. Fully collateralized and
-            backed 1:1 with USD, DD promises stability and transparency,
-            offering users a dependable, secure, and convenient digital
-            currency. The Digital Dollar Funds are never overleveraged, and
-            public API access to the Corporate Bank account assures that the
-            Fiat Balance matches the Digital Dollar total supply at all times..
-          </Text>
-          <h3>Karma</h3>
-          <Text as="p" style={{ color: "#ddd" }}>
-            Alongside DD, we also employ Karma, a native token without monetary
-            value. It cannot be bought, sold, or transferred, but rewards
-            participants contributing to the platform. It serves as a
-            transaction fee and grants VIP access to certain benefits within
-            ecosystems such as communities or events.
-          </Text>
-          <h3>Auditor Rewards</h3>
-          <Text as="p" style={{ color: "#ddd" }}>
-            To maintain the highest standards, we provide auditors and review
-            committee members with a portion of contract revenues, further
-            fortified by our robust infrastructure. In case of contract failure,
-            penalties apply, motivating meticulous review and oversight
-          </Text>
-          <h3>Experience the Digital Dollar</h3>
-          <Text as="p" style={{ color: "#ddd" }}>
-            We are currently privately live, offering a sneak peek into the
-            future through our platformk. Experience now the future generation
-            of digital currency, amplified by the DDBC.
-          </Text>
+
+          <div className="row">
+            <div className="col-md-6 col-12">
+              <div className="h-100">
+
+                <div className="d-flex justify-content-center">
+                  <div className="">
+                    <img src="" alt="" />
+                  </div>
+                  <h3>Digital Dollar (DD)</h3>
+
+
+                </div>
+                <Text as="p" style={{ color: "#ddd" }}>
+                  The DD is a digital asset uniting the stability of the US Dollar
+                  with the prowess of blockchain technology. Fully collateralized and
+                  backed 1:1 with USD, DD promises stability and transparency,
+                  offering users a dependable, secure, and convenient digital
+                  currency. The Digital Dollar Funds are never overleveraged, and
+                  public API access to the Corporate Bank account assures that the
+                  Fiat Balance matches the Digital Dollar total supply at all times..
+                </Text>
+              </div>
+            </div>
+            <div className="col-md-6 col-12">
+              <div className="h-100">
+
+                <div className="d-flex justify-content-center">
+                  <div className="">
+                    <img src="" alt="" />
+                  </div>
+                  <h3>Karma</h3>
+
+
+                </div>
+                <Text as="p" style={{ color: "#ddd" }}>
+                  Alongside DD, we also employ Karma, a native token without monetary
+                  value. It cannot be bought, sold, or transferred, but rewards
+                  participants contributing to the platform. It serves as a
+                  transaction fee and grants VIP access to certain benefits within
+                  ecosystems such as communities or events.
+                </Text>
+              </div>
+            </div>
+            <div className="col-md-6 col-12">
+              <div className="h-100">
+
+                <div className="d-flex justify-content-center">
+                  <div className="">
+                    <img src="" alt="" />
+                  </div>
+                  <h3>Auditor Rewards</h3>
+
+
+                </div>
+                <Text as="p" style={{ color: "#ddd" }}>
+                  To maintain the highest standards, we provide auditors and review
+                  committee members with a portion of contract revenues, further
+                  fortified by our robust infrastructure. In case of contract failure,
+                  penalties apply, motivating meticulous review and oversight
+                </Text>
+              </div>
+            </div>
+            <div className="col-md-6 col-12">
+              <div className="h-100">
+
+                <div className="d-flex justify-content-center">
+                  <div className="">
+                    <img src="" alt="" />
+                  </div>
+                  <h3>Experience the Digital Dollar</h3>
+
+
+                </div>
+                <Text as="p" style={{ color: "#ddd" }}>
+                  We are currently privately live, offering a sneak peek into the
+                  future through our platformk. Experience now the future generation
+                  of digital currency, amplified by the DDBC.
+                </Text>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </Animator>
